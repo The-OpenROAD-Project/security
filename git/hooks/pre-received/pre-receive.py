af60c0fff6821fb528b5c35a1cb423baead0fcc6
openroadadmin <tomwspyrou@gmail.com>
2020-08-06 14:02:38 -0700

    Update pre-receive.py

--- git/hooks/pre-received/pre-receive.py
+++ git/hooks/pre-received/pre-receive.py
@@ -1,5 +1,12 @@
-import sys 
-  
+#!/usr/bin/env python3
+
+import argparse
+import subprocess
+import re
+import sys
+import os
+
+print("Running pre-commit security hook....")import sys 
   
 for line in sys.stdin: 
     if 'q' == line.rstrip(): 
