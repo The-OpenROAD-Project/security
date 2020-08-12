444c83889d3e8ee8a562848ae339098051d995c7
Tom Spyrou <46945466+tspyrou@users.noreply.github.com>
2020-08-12 12:02:41 -0700

    incremental progress pulling all branches from public github repo

--- merging/merge_from_public.py
+++ merging/merge_from_public.py
@@ -6,6 +6,14 @@ import argparse
 import os
 import shlex
 
+# See for recipe to clone repo with all branches to new remote
+# https://gist.github.com/niksumeiko/8972566
+
+def remove_prefix(text, prefix):
+    if text.startswith(prefix):
+        return text[len(prefix):]
+    return text
+
 def which(pgm):
     path=os.getenv('PATH')
     for p in path.split(os.path.pathsep):
@@ -22,10 +30,11 @@ def check_exists(pgm):
 
 def run_command_locally(command):
     print("command=",command)
-    print(subprocess.run(shlex.split(command), stdout=subprocess.PIPE).stdout.decode('utf-8'))
-
-if not check_exists("git"):
-    exit(0)
+    retval = subprocess.run(shlex.split(command), stdout=subprocess.PIPE).stdout.decode('utf-8')
+    print(retval)
+    if not check_exists("git"):
+        exit(0)
+    return(retval)
 
 github_remote_prefix = "git@github.com:The-OpenROAD-Project/"
 gite_remote_prefix = "git@gite.openroad.tools:The-OpenROAD-Project-Private/"
@@ -40,8 +49,21 @@ print("running in working dir ", work_dir)
 print("path to script=",os.path.dirname(os.path.realpath(__file__)))
 for repo in repo_names:
     print("repo=",repo)
-    run_command_locally("git clone --branch openroad " + github_remote_prefix + repo)
+    run_command_locally("git clone " + github_remote_prefix + repo)
     os.chdir(repo.split(".")[0])
+    # fetch all branches
+    run_command_locally("git fetch origin")
+    branches_raw_nosplit = run_command_locally("git branch -a")
+    # First line is *master, second line is pointer to master remote
+    branches_raw_split = branches_raw_nosplit.split("\n")[2:-1]
+    branches = [item.strip() for item in branches_raw_split]
+    print(branches)
+    for branch in branches:
+        branch = remove_prefix(branch,"remotes/origin/")
+        print("working on " + branch)
+        run_command_locally("git checkout -b " + branch)
+        #pull from new origin gite, automatically merges
+        run_command_locally("git pull origin/" + branch + " " + branch)
 
     #origin remote is github
     run_command_locally("git remote -v")
@@ -49,13 +71,10 @@ for repo in repo_names:
     #change origin to gite
     run_command_locally("git remote set-url origin " + gite_remote_prefix + repo)
 
-    #gite remote is gite.openroad.tools
+    #remote is gite.openroad.tools
     run_command_locally("git remote -v")
 
-    #pull from new origin gite, automatically merges
-    run_command_locally("git pull")
-
     #push to origin which is gite
-    run_command_locally("git push")
+    #run_command_locally("git push --all origin")
     
 
---
+++ merging/merge_to_public.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+import sys
+import subprocess
+import array as arr
+import argparse
+import os
+import shlex
+
+def which(pgm):
+    path=os.getenv('PATH')
+    for p in path.split(os.path.pathsep):
+        p=os.path.join(p,pgm)
+        if os.path.exists(p) and os.access(p,os.X_OK):
+            return p
+
+def check_exists(pgm):
+    if which(pgm) == None:
+        print(pgm, "not on path")
+        return False
+    return True
+
+
+def run_command_locally(command):
+    print("command=",command)
+    print(subprocess.run(shlex.split(command), stdout=subprocess.PIPE).stdout.decode('utf-8'))
+
+if not check_exists("git"):
+    exit(0)
+
+github_remote_prefix = "git@github.com:The-OpenROAD-Project/"
+gite_remote_prefix = "git@gite.openroad.tools:The-OpenROAD-Project-Private/"
+repo_names = [
+    "OpenDB.git",
+#    "OpenROAD.git",
+#    "OpenROAD-flow.git",
+#    "TritonRoute.git"
+    ]
+work_dir = os.getcwd()
+print("running in working dir ", work_dir)
+print("path to script=",os.path.dirname(os.path.realpath(__file__)))
+for repo in repo_names:
+    print("repo=",repo)
+    run_command_locally("git clone " + gite_remote_prefix + repo)
+    os.chdir(repo.split(".")[0])
+
+    #origin remote is gite
+    run_command_locally("git remote -v")
+
+    #change origin to github
+    run_command_locally("git remote set-url origin " + github_remote_prefix + repo)
+
+    #remote is github
+    run_command_locally("git remote -v")
+
+    #pull from new origin github, automatically merges
+    run_command_locally("git pull")
+
+    #push to origin which is github
+    run_command_locally("git push --all origin")
+    
