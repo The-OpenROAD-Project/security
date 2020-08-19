4956b34e59fdbdb07809a866dcd715f96fa5d22a
Tom Spyrou <46945466+tspyrou@users.noreply.github.com>
2020-08-19 16:25:12 +0000

    refactoring

--- merging/merge_from_public.py
+++ merging/merge_from_public.py
@@ -5,20 +5,18 @@ import array as arr
 import os
 import shlex
 
-def run_command_locally(command):
-    print("command=",command)
-    retval = subprocess.run(shlex.split(command), stdout=subprocess.PIPE).stdout.decode('utf-8')
-    print(retval)
-    return(retval)
+work_dir = os.getcwd()
+path_to_script = os.path.dirname(os.path.realpath(__file__))
+print("running in working dir ", work_dir)
+print("path to script=",os.path.dirname(os.path.realpath(__file__)))
+for path in sys.path:
+    print(path)
+sys.path.append(path_to_script)
+import utils
 
 github_remote_prefix = "git@github.com:The-OpenROAD-Project/"
-gite_remote_prefix = "git@gite.openroad.tools:The-OpenROAD-Project-Private/"
+gite_remote_prefix = "git@github.com:The-OpenROAD-Project-Private/"
 repo_names = "OpenDB.git OpenROAD.git"
 
-work_dir = os.getcwd()
-print("running in working dir ", work_dir)
-path_to_script = os.path.dirname(os.path.realpath(__file__))
-print("path to script=",path_to_script)
-
 script_command = path_to_script + "/" + "merge_from_to_remote.py --from_remote " + github_remote_prefix + " --to_remote " + gite_remote_prefix + " --repo_names " + repo_names +" --repo_branches master openroad"
-print(run_command_locally(script_command))
+print(utils.run_command_locally(script_command))
--- merging/merge_from_to_remote.py
+++ merging/merge_from_to_remote.py
@@ -9,33 +9,15 @@ import shlex
 # See for recipe to clone repo with all branches to new remote
 # https://gist.github.com/niksumeiko/8972566
 
-def remove_prefix(text, prefix):
-    if text.startswith(prefix):
-        return text[len(prefix):]
-    return text
+work_dir = os.getcwd()
+path_to_script = os.path.dirname(os.path.realpath(__file__))
+print("running in working dir ", work_dir)
+print("path to script=",os.path.dirname(os.path.realpath(__file__)))
+for path in sys.path:
+    print(path)
+sys.path.append(path_to_script)
+import utils
 
-def which(pgm):
-    path=os.getenv('PATH')
-    for p in path.split(os.path.pathsep):
-        p=os.path.join(p,pgm)
-        if os.path.exists(p) and os.access(p,os.X_OK):
-            return p
-
-def check_exists(pgm):
-    if which(pgm) == None:
-        print(pgm, "not on path")
-        return False
-    return True
-
-def run_command_locally(command):
-    print("command=",command)
-    retval = subprocess.run(shlex.split(command), stdout=subprocess.PIPE).stdout.decode('utf-8')
-    print(retval)
-    if not check_exists("git"):
-        exit(0)
-    return(retval)
-
-import argparse
 
 # Parse and check arguments
 parser = argparse.ArgumentParser(description='Merge all changes from a remote to a remote for a repo list')
@@ -52,31 +34,28 @@ repo_names = args.repo_names
 repo_branches = args.repo_branches
 print("from_remote_prefix=", from_remote_prefix)
 print("to_remote_prefix=", to_remote_prefix)
-print("repo_name=", repo_names)
+print("repo_names=", repo_names)
 print("repo_branches=", repo_branches)
 
 
-work_dir = os.getcwd()
-print("running in working dir ", work_dir)
-print("path to script=",os.path.dirname(os.path.realpath(__file__)))
 for repo in repo_names:
     print("repo=",repo)
-    run_command_locally("git clone " + from_remote_prefix + repo)
+    utils.run_command_locally("git clone " + from_remote_prefix + repo)
     os.chdir(repo.split(".")[0])
     # fetch all branches
-    run_command_locally("git pull")
+    utils.run_command_locally("git pull")
     branches = repo_branches
     print(branches)
     for branch in branches:
-        branch = remove_prefix(branch,"remotes/origin/")
+        branch = utils.remove_prefix(branch,"remotes/origin/")
         print("working on " + branch)
-        run_command_locally("git checkout -f " + branch)
+        utils.run_command_locally("git checkout -f " + branch)
         #pull from new origin gite, automatically merges
-        run_command_locally("git pull origin " + branch)
+        utils.run_command_locally("git pull origin " + branch)
 
-    run_command_locally("git remote -v")
-    run_command_locally("git remote set-url origin " + to_remote_prefix + repo)
-    run_command_locally("git remote -v")
-    run_command_locally("git pull")
-    print(run_command_locally("git push --all origin"))
+    utils.run_command_locally("git remote -v")
+    utils.run_command_locally("git remote set-url origin " + to_remote_prefix + repo)
+    utils.run_command_locally("git remote -v")
+    utils.run_command_locally("git pull")
+    print(utils.run_command_locally("git push --all origin"))
     os.chdir("..")    
