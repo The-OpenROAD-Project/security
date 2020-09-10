2535fa1405756e2ec742faaefee246534e891299
Tom Spyrou <46945466+tspyrou@users.noreply.github.com>
2020-09-10 14:21:34 -0700

    update demo of pygithub

--- merging/py_github_demo.py
+++ merging/py_github_demo.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python3
+import os
 from github import Github
 
-# First create a Github instance:
 
-# using username and password
-# g = Github("user", "password")
-
-f = open('/home/tspyrou/git_merge_token')
+from os.path import expanduser
+home = expanduser("~")
+token_file = home + "/" + "git_merge_token"
+f = open(token_file)
 access_token = f.readline().strip()
 f.close()
 
-# or using an access token
+# First create a Github instance using an access token
+# with needed privs
 g = Github(access_token)
 
 user = g.get_user()
 print(user.login)
 print(user.name)
 
+org = g.get_organization('The-OpenROAD-Project')
+print(org.url)
 
-# Github Enterprise with custom hostname
-#g = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
+repo = org.get_repo('OpenROAD')
+print(repo.full_name)
 
-# Then play with your Github objects:
-for repo in g.get_user().get_repos():
-    print(repo.name)
-    print(repo.full_name)
+
