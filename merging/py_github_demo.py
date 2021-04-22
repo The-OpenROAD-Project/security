37f21ebeac781ccf78aebd0cb00fe985e307f7f1
Tom Spyrou <46945466+tspyrou@users.noreply.github.com>
2021-04-22 14:23:28 -0700

    Ready to sync public label check working on PR

--- merging/py_github_demo.py
+++ merging/py_github_demo.py
@@ -18,7 +18,7 @@ user = g.get_user()
 print(user.login)
 print(user.name)
 
-org = g.get_organization('The-OpenROAD-Project')
+org = g.get_organization('The-OpenROAD-Project-private')
 print(org.url)
 
 repo = org.get_repo('OpenROAD')
@@ -26,7 +26,7 @@ print(repo.full_name)
 
 branches = repo.get_branches()
 for branch in branches:
-    if branch.name == "openroad":
+    if branch.name == "master":
         print(branch.name)
         print(branch.protection_url)
         print(branch.get_protection().enforce_admins)
@@ -34,5 +34,27 @@ for branch in branches:
         print(branch.get_protection().enforce_admins)
         print(branch.set_admin_enforcement())
         print(branch.get_protection().enforce_admins)
+        print(branch.get_required_status_checks())
+
+        
+pulls = repo.get_pulls(state="open")
+for pull in pulls:
+    title = pull.title
+    number = pull.number
+    merged = pull.merged
+    mergeable = pull.mergeable
+    mergeable_state = pull.mergeable_state
+    user = pull.user
+    state = pull.state
+    milestone = pull.milestone
+    review_comment_url = pull.review_comment_url
+    draft = pull.draft
+    update_branch = pull.update_branch()
+    labels = pull.labels
+    print("{} {} {} {} {} {} {} {} {} {}".format(number,merged,mergeable,mergeable_state,user, state, milestone, draft, update_branch, labels))
+    for l in labels:
+        print("l={} {}".format(l.name, l.name == "Ready To Sync Public"))
+          
+    
 
 
