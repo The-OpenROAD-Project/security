c23e5363e6120aa7e5f05cd64f75826cafaeac95
Tom Spyrou <46945466+tspyrou@users.noreply.github.com>
2021-04-27 15:52:48 -0700

    user agent header added

--- merging/gitapi_demo.py
+++ merging/gitapi_demo.py
@@ -4,7 +4,7 @@ from os.path import expanduser
 import urllib3
 import json
 
-
-http = urllib3.PoolManager()
+user_agent = {'user-agent': 'Mozilla/5.0 (Windows NT 6.3; rv:36.0) Gecko/20100101 Firefox/36.0'}
+http = urllib3.PoolManager(10, user_agent)
 r = http.request("GET","https://api.github.com/repos/tspyrou/OpenROAD/commits/master/status")
 print(r.data)
