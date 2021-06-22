de41ceb2f6b74a9415065c37617214f673e0e41b
Matt Liberty <mliberty@eng.ucsd.edu>
2021-06-21 21:14:15 -0700

    reset file_change_limit = 50

--- git/hooks/pre-commit.py
+++ git/hooks/pre-commit.py
@@ -12,7 +12,7 @@ print("Running pre-commit security hook....")
 
 # Limits for rules
 file_add_limit    = 10
-file_change_limit = 300
+file_change_limit = 50
 
 # The following patterns are regular expressions and are matched
 # case-insensitive.  They are matched anywhere with the string
