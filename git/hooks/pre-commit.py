8eaf1caf969f6bbc37bce000d53bcbb59fae7a60
Matt Liberty <mliberty@eng.ucsd.edu>
2021-10-20 15:39:35 -0700

    add config_hier.mk to the whitelist
    Signed-off-by: Matt Liberty <mliberty@eng.ucsd.edu>

--- git/hooks/pre-commit.py
+++ git/hooks/pre-commit.py
@@ -131,6 +131,7 @@ skip_content_patterns = [
     r"^\.git/",
     r"^flow/designs/.*/metadata.*-ok.json$",
     r"^flow/designs/.*/config.mk$",
+    r"^flow/designs/.*/config_hier.mk$",
     r"^flow/designs/.*/wrappers.tcl$",
     r"^flow/designs/.*/macros.v$",
     r"^flow/designs/src/.*\.sv2v\.v$",
