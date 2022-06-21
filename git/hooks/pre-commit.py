74493dbeef7a09cfdb29d18be0299515a22a6ae5
Vitor Bandeira <vitor.vbandeira@gmail.com>
2022-06-20 17:17:10 -0700

    Add config_synth.mk exception
    Signed-off-by: Vitor Bandeira <vitor.vbandeira@gmail.com>

--- git/hooks/pre-commit.py
+++ git/hooks/pre-commit.py
@@ -137,6 +137,7 @@ skip_content_patterns = [
     r"^flow/designs/.*/metadata.*-ok.json$",
     r"^flow/designs/.*/config.mk$",
     r"^flow/designs/.*/config_hier.mk$",
+    r"^flow/designs/.*/config_synth.mk$",
     r"^flow/designs/.*/wrappers.tcl$",
     r"^flow/designs/.*/macros.v$",
     r"^flow/designs/src/.*\.sv2v\.v$",
