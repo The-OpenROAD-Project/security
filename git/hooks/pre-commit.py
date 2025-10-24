a62a8ba78ce9a92ed017ee6a5359ccba46ad2821
Matt Liberty <mliberty@precisioninno.com>
2025-10-24 11:06:44 +0000

    Add MODULE.bazel to skip_content_patterns
    Signed-off-by: Matt Liberty <mliberty@precisioninno.com>

--- git/hooks/pre-commit.py
+++ git/hooks/pre-commit.py
@@ -161,6 +161,7 @@ skip_content_patterns = [
     r"^(tools/OpenROAD/)?src/sta/README.md$",
     r"^(tools/OpenROAD/)?src/sta/examples/gcd_sky130hd.vcd.gz$",
     r"^(tools/OpenROAD/)?third-party/abc",
+    r"^(tools/OpenROAD/)?MODULE.bazel",
     r"^(flow/platforms/)?sky130hd/chameleon",
     r"^(flow/designs/)?sky130hd/chameleon",
     r"^tools/yosys/",
